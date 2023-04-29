d9b7e9c66f85345dabad975fb7d0a6013a26a4ac
GOVIND-BOTS <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-04-30 01:50:38 +0530

    Add files via upload

---
+++ MukeshRobot/modules/alive.py
@@ -0,0 +1,71 @@
+import asyncio
+from platform import python_version as pyver
+
+from pyrogram import __version__ as pver
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telegram import __version__ as lver
+from telethon import __version__ as tver
+
+from MukeshRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID
+
+PHOTO = [
+    "https://telegra.ph/file/d2a23fbe48129a7957887.jpg",
+    "https://telegra.ph/file/ddf30888de58d77911ee1.jpg",
+    "https://telegra.ph/file/268d66cad42dc92ec65ca.jpg",
+    "https://telegra.ph/file/13a0cbbff8f429e2c59ee.jpg",
+    "https://telegra.ph/file/bdfd86195221e979e6b20.jpg",
+]
+
+Mukesh = [
+    [
+        InlineKeyboardButton(text="📍𝐎𝐰𝐧𝐞𝐫📍", url=f"tg://user?id={OWNER_ID}"),
+        InlineKeyboardButton(text="🍒𝐆𝐫𝐨𝐮𝐩🍒", url=f"https://t.me/{SUPPORT_CHAT}"),
+    ],
+    [
+        InlineKeyboardButton(
+            text="☆ 𝐀𝐝𝐝 𝐌𝐞 𝐌𝐨𝐢 𝐋𝐮𝐯 ☆",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+]
+
+lol = "https://graph.org/file/d874fc40ddf9ead0cad6b.jpg"
+
+
+@pbot.on_message(filters.command("alive"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("⚡")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
+    await accha.delete()
+    await asyncio.sleep(0.5)
+    umm = await m.reply_sticker(
+        "CAACAgUAAxkDAAJHbmLuy2NEfrfh6lZSohacEGrVjd5wAAIOBAACl42QVKnra4sdzC_uKQQ"
+    )
+    await umm.delete()
+    await asyncio.sleep(0.8)
+    await m.reply_photo(
+        lol,
+        caption=f"""**🌷ʜᴇʏ, ɪ ᴀᴍ 『[𝗩𝗜𝗣 𝗥𝗢𝗕𝗢𝗧](f"t.me/{BOT_USERNAME}")』🎄**
+   ╔═════ஜ۩۞۩ஜ════╗
+
+   ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 [𝗩𝗜𝗣 𝗕𝗢𝗬](https://t.me/THE_VIP_BOY)♨️
+
+   ╚═════ஜ۩۞۩ஜ════╝""",
+        reply_markup=InlineKeyboardMarkup(Mukesh),
+    )
+__mod_name__ = "♨️ᴀʟɪᴠᴇ♨️"
+__help__ = """
+
+*ᴜsᴇʀ ᴄᴏᴍᴍᴀɴᴅs*:
+» /alive*:* ᴛᴏ ᴄʜᴇᴀᴋ ❓  ɪ ᴀᴍ ᴀʟɪᴠᴇ ᴏʀ ɴᴏᴛ?
+
+☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆"""
